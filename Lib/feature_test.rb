a04b795707d9a765fbf9e93a0ac887050b33fc3a
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-01-08 16:51:05 +0000

    unit & feature test

--- Lib/feature_test.rb
+++ Lib/feature_test.rb
@@ -5,9 +5,20 @@ class DockingStation
   # end
 
   def release_bike
+    # if bike == working then release_bike
+    # else
+    #   break
+    # CODE TO TEST IF BIKE RELEASED
+    # end
   end
+
+  #   def working_bike
+  #     # code to test if working
+  #   end
 end
 
 docking_station = DockingStation.new
 
 bike = docking_station.release_bike
+
+# working = docking_station.working_bike
