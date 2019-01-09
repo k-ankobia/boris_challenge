6e2da6bff3dc0323bd2e307d7a31f84fba8c841a
ruby <ruby@rubykobayashi.com>
2019-01-09 17:33:20 +0000

    Passing test for bike.new

--- Lib/docking_station.rb
+++ Lib/docking_station.rb
@@ -1,8 +1,10 @@
 #A Domain Model is an abstract representation of the Objects within a system
+require 'bike'
 class DockingStation
 
 
   def release_bike
+    Bike.new
   end
 
 end
--- spec/docking_Station_spec.rb
+++ spec/docking_Station_spec.rb
@@ -3,11 +3,13 @@ require "docking_station.rb"
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-  end
 
 
-
-
+it "releases working bikes " do
+  bike = subject.release_bike
+   expect(bike).to be_working
+end
+end
 
 
 
