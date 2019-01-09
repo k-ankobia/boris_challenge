18c8cdaded294678510577e3e46a115f5b821592
ruby <ruby@rubykobayashi.com>
2019-01-09 15:01:22 +0000

    Step 8

---
+++ Lib/docking_station.rb
@@ -0,0 +1,12 @@
+#A Domain Model is an abstract representation of the Objects within a system
+class DockingStation
+
+
+  # def release_bike
+  # end
+
+end
+
+docking_station = DockingStation.new
+
+bike = docking_station.release_bike
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,4 +1,4 @@
-require "/Users/apple/Projects/boris_challenge/Lib/bike.rb"
-describe Bike do
-  it { is_expected.to respond_to :working? }
-end
+# require "/Users/apple/Projects/boris_challenge/Lib/bike.rb"
+# describe Bike do
+#   it { is_expected.to respond_to :working? }
+# end
